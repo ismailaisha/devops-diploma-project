cea2e3bf8950bfd1696ab7a94e24b4a67623cd2b
Aishan <tabladance@mail.ru>
2026-07-20 18:39:50 -0500

    update docker-compose to use Docker Hub images

--- infrastructure/terraform/environments/dev/providers.tf
+++ infrastructure/terraform/environments/dev/providers.tf
@@ -1,20 +1,19 @@
 terraform {
-    required_version = ">= 1.0"
+  required_version = ">= 1.0"
 
-required_providers {
-  aws = {
-    source = "hashicorp/aws"
-    version = "~> 5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.0"
+    }
   }
-  http = {
-    source  = "hashicorp/http"
-    version = "~> 3.0"
-  }
-}
 }
 
 provider "aws" {
-    region = var.aws_region
-    profile = "aisha-devops"
-  
+  region  = var.aws_region
+  profile = "aisha-devops"
 }
